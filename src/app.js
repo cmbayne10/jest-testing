f2a3a543d5ebe424c78ee395878bf46cbba01743
cmbayne10 <cmbayne10@gmail.com>
2024-03-08 07:39:30 -0500

    laptop commit

--- src/app.js
+++ src/app.js
@@ -1,6 +1,24 @@
 export function add(a, b) {
-    return a + b;
+  return a + b;
 }
 export function subtract(a, b) {
-    return a - b;
+  return a - b;
+}
+export function multiply(a, b) {
+  return a * b;
+}
+export function divide(a, b) {
+  return a / b;
+}
+function fizzbuzz(a) {
+  it("fizz buzz returns 1", () => {
+    expect(fizzbuzz(1)).toEqual(1);
+  });
+  it("fizz buzz returns fizz", () => {
+    expect(fizzbuzz(5)).toEqual(fizz);
+  });
+  it("fizz buzz returns buzz", () => {
+    expect(fizzbuzz(3)).toEqual(buzz);
+  });
+  
 }
--- src/app.test.js
+++ src/app.test.js
@@ -1,4 +1,4 @@
-import { add, subtract } from "./app";
+import { add, subtract, multiply, divide } from "./app";
 
 describe("my test suite", () => {
   it("adds 1+1", () => {
@@ -7,4 +7,11 @@ describe("my test suite", () => {
   it("subtracts 2-1", () => {
     expect(subtract(2, 1)).toEqual(1);
   });
+  it("multiplies 510948*25834", () => {
+    expect(multiply(510948, 25834)).toEqual(13199830632);
+  });
+  it("divide 2442468/4", () => {
+    expect(divide(2442468, 4)).toEqual(610617);
+  });
+ 
 });
